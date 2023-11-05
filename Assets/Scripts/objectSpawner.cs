90a28827c94418ca82dbcbfbd9e2a3e62785c61a
LianLambert <lian1lambert@gmail.com>
2023-11-04 21:05:04 -0400

    created ObjectSpawner

--- Assets/Scripts/objectSpawner.cs
+++ Assets/Scripts/objectSpawner.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class objectSpawner : MonoBehaviour
 {
+    [SerializeField] private GameObject chair;
+    [SerializeField] private GameObject human;
+    [SerializeField] private GameObject goal;
+    [SerializeField] public int numChairs;
+    [SerializeField] public int numHumans;
+    private int minX = -24;
+    private int maxX = 23;
+    private int minZ = -49;
+    private int maxZ = 48;
+    private List<Vector3> usedPositions = new List<Vector3>();
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPrefab(chair, numChairs);
+        spawnPrefab(human, numHumans);
+        spawnPrefab(goal, 1);
     }
 
     // Update is called once per frame
-    void Update()
+    void spawnPrefab(GameObject prefab, int numPrefabs)
     {
-        
+        for (int i = 0; i<numPrefabs; i++)
+        {
+            Vector3 spawnPos = GetRandomIntegerPosition();
+
+            while (usedPositions.Contains(spawnPos))
+            {
+                spawnPos = GetRandomIntegerPosition();
+            }
+
+            usedPositions.Add(spawnPos);
+
+            if (prefab == human)
+            {
+                spawnPos = spawnPos + new Vector3(0, 1, 0);
+            }
+
+            Instantiate(prefab, spawnPos, Quaternion.identity);
+        }   
+    }
+
+    private Vector3 GetRandomIntegerPosition()
+    {
+        int randomX = Random.Range(minX, maxX + 1);
+        int randomZ = Random.Range(minZ, maxZ + 1);
+
+        return new Vector3(randomX, 1, randomZ);
     }
 }
